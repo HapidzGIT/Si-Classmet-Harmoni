921faf18f500791f9069e1f5a39613b709aef7f5
UserHan9 <raihanindra712@gmail.com>
2024-01-16 14:39:04 +0700

    Login Multi user

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,24 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        $user = auth()->user();
 
-Route::middleware('auth')->group(function () {
+     
+        if ($user) {
+            if ($user->usertype === 'user') {
+                return Inertia::render('user/UserComponent'); // Sesuaikan dengan struktur direktori Anda
+            } elseif ($user->usertype === 'admin') {
+                return Inertia::render('admin/AdminComponent'); // Sesuaikan dengan struktur direktori Anda
+            }
+        }
+
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
 require __DIR__.'/auth.php';
